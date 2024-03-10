e7c83c9300239794af95189c02ecacb4c8ad7e64
Abinayas-14 <abisaran149@gmail.com>
2024-03-10 16:04:36 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { LoginSingnup } from './Components/LoginSignup/LoginSingnup';
+
+function App() {
+  return (
+    <div>
+      <LoginSingnup/>
+    </div>
+  );
+}
+
+export default App;
